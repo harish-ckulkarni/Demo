4d2ea0461f58fd57748bdc958927e72650c77fa6
harish-ckulkarni <harish.kulkarni81@gmail.com>
2022-08-18 17:23:55 +0530

    String lines are added

--- src/test/java/Demo7.java
+++ src/test/java/Demo7.java
@@ -1,7 +1,12 @@
+import java.util.Locale;
+
 public class Demo7 {
 
     public static void main(String args[])
     {
+        String s1="TESTING";
+        String s2=s1.toLowerCase(Locale.ROOT);
+        System.out.println(s2);
         System.out.println("demo7");
     }
 }
